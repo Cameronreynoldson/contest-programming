74a38b9f6df7b8defef762349b7b05eefba8d4b5
Cameron Reynoldson <cameronreynoldson@utexas.edu>
2015-11-10 13:19:44 -0600

    lol

---
+++ project-euler/10001prime.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+
+public class 10001prime
+{
+    public static void main(String[] args)
+    {
+        int n = 10001; 
+
+        prime(n);
+    }
+
+    public static int prime(int n)
+    {
+        
+    }
+}
